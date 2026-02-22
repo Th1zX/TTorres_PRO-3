efc8450fbb08b57430ee97a056aa73388eae8c05
Th1zX <torrestaddheo@gmail.com>
2026-02-21 21:18:22 -0700

    del trash

--- src/Vent.java
+++ src/Vent.java
@@ -4,19 +4,18 @@ import javax.swing.*;
 public class Vent extends JFrame{
 
     public Vent() {
-        this.setTitle("X");
+        this.setTitle("Login");
+        this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLocationRelativeTo(null); 
-        this.setVisible(true);
-        this.setIconImage(new ImageIcon("src/changopedo.jpg").getImage());
-        
+        this.setVisible(true);        
         JLabel tag_title = new JLabel();
 
-        tag_title.setText("X");
+        tag_title.setText("");
         tag_title.setSize(500,500);
         tag_title.setLocation(0,0);
-        tag_title.setBackground(Color.black);
+        tag_title.setBackground(Color.white);
         tag_title.setOpaque(true);
         tag_title.setHorizontalAlignment(JLabel.CENTER);
         tag_title.setVerticalAlignment(JLabel.CENTER);
